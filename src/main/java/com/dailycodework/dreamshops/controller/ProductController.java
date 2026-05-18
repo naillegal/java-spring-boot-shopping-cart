9fa4c843ff71da3fefa62e94d135f8baeb8f3a28
nail.guluzada <nail.guluzada@prodata-di.az>
2026-05-18 15:06:06 +0400

    ProductController started

--- src/main/java/com/dailycodework/dreamshops/controller/ProductController.java
+++ src/main/java/com/dailycodework/dreamshops/controller/ProductController.java
@@ -1,4 +1,22 @@
 package com.dailycodework.dreamshops.controller;
 
+import com.dailycodework.dreamshops.model.Product;
+import com.dailycodework.dreamshops.response.ApiResponse;
+import com.dailycodework.dreamshops.service.product.IProductService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/products")
 public class ProductController {
+    private final IProductService productService;
+
+    public ResponseEntity<ApiResponse> getAllProducts(){
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(new ApiResponse("success", products));
+    }
 }
